e7503b0abba26bf68872c8391f937354dc11d774
Lakshit Dabas <lakshitsdabas@gmail.com>
2019-02-24 21:28:05 +0000

    Created Server

--- chat.py
+++ chat.py
@@ -1,6 +1,30 @@
 import socket
 import threading
-import numpy
-import turtle
 
-print("Chat Client")
+class Server:
+
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	connections = []
+	def __init__(self):
+		self.sock.bind(('0.0.0.0', 10000))
+		self.sock.listen(1)
+
+	def handler(self, c, a):
+		while True:
+			data = c.recv(1024)
+			for connection in self.connections:
+				connection.send(data)
+			if not data:
+				break
+
+	def run(self): 
+		while True:
+			c,a = self.sock.accept()
+			cThread = threading.Thread(target=self.handler, args=(c,a ))
+			cThread.daemon = True
+			cThread.start()
+			self.connections.append(c)
+			print(self.connections)
+
+server = Server()
+server.run()
